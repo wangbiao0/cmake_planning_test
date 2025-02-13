272cdeb8a671d8707a36e8c07fe17419dd220fde
wangbiao0 <1477094433@qq.com>
2025-02-13 10:23:02 +0800

    add test

---
+++ test/map_test/map_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "pnc_map.h"
+
+void pncMapTest(){
+    std::cout<<"this is pnc_map test !"<<std::endl;
+    PNC_MAP mymap;
+    mymap.mapInfo();
+}
+
+int main(){
+
+    pncMapTest();
+    return 0;
+}
